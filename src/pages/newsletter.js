578d2dfc6d991c841ceb971deb323304af582487
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-08-27 02:55:53 +0530

    newsletter form created

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import Layout from '../components/Layout'
+
 const NewsLetter = () => {
   return (
-    <h4>newsletter</h4>
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get all the latest stories to your inbox!</h2>
+          <h4>I write to my digital friends every few weeks.</h4>
+
+          <form className="contact-form">
+            <input
+              type="text"
+              name="Name"
+              placeholder="Your name"
+              className="form-control"
+            />
+            <input
+              type="email"
+              name="Email"
+              placeholder="Your email"
+              className="form-control"
+            />
+            <button type="submit" className="btn form-control submit-btn">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
   )
 }
 
