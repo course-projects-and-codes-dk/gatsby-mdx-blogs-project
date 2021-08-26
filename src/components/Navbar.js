a624afbaf1e5b981943d2b16104a6922f066f4a0
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-08-27 00:17:19 +0530

    navbar created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,26 @@ import { FaBars } from 'react-icons/fa'
 import logo from '../assets/logo.svg'
 import Links from '../constants/links'
 import SocialLinks from '../constants/socialLinks'
+
 const Navbar = () => {
   return (
-    <h4>navbar component</h4>
+    <nav className="navbar">
+      <div className="nav-center">
+        {/* header */}
+        <div className="nav-header">
+          <Link to="/" className="nav-log">
+            <img src={logo} alt="MDX Logo" />
+          </Link>
+          <button className="toggle-btn">
+            <FaBars />
+          </button>
+        </div>
+
+        {/* links */}
+        <Links styleClass="nav-links" />
+        <SocialLinks styleClass="nav-icons" />
+      </div>
+    </nav>
   )
 }
 
